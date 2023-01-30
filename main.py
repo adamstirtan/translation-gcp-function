b75be62c6fef01a55041494d8623cb05a04388ce
Adam Stirtan <adamstirtan@gmail.com>
2023-01-29 22:58:01 -0500

    Formats as Slack message

--- main.py
+++ main.py
@@ -7,6 +7,36 @@ from slack.signature import SignatureVerifier
 
 translate_client = translate.Client()
 
+def format_slack_message(text, translated_text, target_language, source_language):
+    message = {
+        'response_type': 'in_channel',
+        'attachments': []
+    }
+
+    attachment = {
+        'color': '#0014d1',
+        'fields': []
+    }
+
+    attachment['pretext'] = 'Translation: {}'.format(text)
+    attachment['text'] = translated_text
+
+    attachment['fields'].append({
+        'title': 'From',
+        'value': source_language,
+        'short': True
+    })
+
+    attachment['fields'].append({
+        'title': 'To',
+        'value': target_language,
+        'short': True
+    })
+
+    message['attachments'].append(attachment)
+
+    return message
+
 def verify_signature(request):
     request.get_data()
 
@@ -19,24 +49,27 @@ def make_translation_request(text, target_language, source_language):
     if (target_language == source_language):
         return text
 
-    translated_text = translate_client.translate(
+    translation = translate_client.translate(
         text, target_language = target_language, source_language = source_language
     )
 
-    return translated_text
+    return translation
 
-def translate(request, language):
+def translate(request, target_language):
     if request.method != 'POST':
         return 'Only POST requests are accepted', 405
 
-    verify_signature(request)
+    #verify_signature(request)
 
     text = request.form['text']
     source_language = translate_client.detect_language(text)
 
-    translation_response = make_translation_request(text, language, source_language['language'])
+    translation = make_translation_request(text, target_language, source_language['language'])
 
-    return jsonify(translation_response)
+    slack_message = format_slack_message(
+        text, translation['translatedText'], target_language, source_language['language'])
+
+    return slack_message
 
 @functions_framework.http
 def to_french(request):
